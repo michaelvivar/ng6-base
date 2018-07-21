ee96c0c0e383f1f92e79c90c402255d90b79cf26
Michael Vivar <michaelvictorvivar@gmail.com>
2018-07-21 12:08:52 +0800

    remove firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyD2E34OEB28H0ZADiND0FeULfKmHx8MWyk",
-    authDomain: "shopping-cart-db.firebaseapp.com",
-    databaseURL: "https://shopping-cart-db.firebaseio.com",
-    projectId: "shopping-cart-db",
-    storageBucket: "shopping-cart-db.appspot.com",
-    messagingSenderId: "451012300747"
+
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyD2E34OEB28H0ZADiND0FeULfKmHx8MWyk",
-    authDomain: "shopping-cart-db.firebaseapp.com",
-    databaseURL: "https://shopping-cart-db.firebaseio.com",
-    projectId: "shopping-cart-db",
-    storageBucket: "shopping-cart-db.appspot.com",
-    messagingSenderId: "451012300747"
+
   }
 };
 
